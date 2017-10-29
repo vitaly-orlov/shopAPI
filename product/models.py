33c19f3ef34d81f0230daf897272cac7a51dec9b
vetal <particlesystem@yandex.ru>
2017-10-29 16:29:28 +0300

    ugetex_lazy

--- product/models.py
+++ product/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 
 class Category(models.Model):
     class Meta:
         db_table = 'category'
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
 
-    name = models.CharField('Название категории', max_length=250)
-    img = models.ImageField('Фото', upload_to='category', blank=True, default='noimage.jpg')
-    description = models.TextField('Описание', blank=True, default='')
-    brands = models.SmallIntegerField('Бренды')
+    name = models.CharField(_('name'), max_length=250)
+    img = models.ImageField(_('photo'), upload_to='category', blank=True, default='noimage.jpg')
+    description = models.TextField(_('description'), blank=True, default='')
+    brands = models.SmallIntegerField(_('brands'))
 
     def __str__(self):
         return self.name
@@ -19,13 +19,13 @@ class Category(models.Model):
 class Brands(models.Model):
     class Meta:
         db_table = 'brands'
-        verbose_name = 'Брэнд'
-        verbose_name_plural = 'Брэнды'
+        verbose_name = _('brand')
+        verbose_name_plural = _('brands')
 
-    name = models.CharField('Имя', max_length=250)
-    img = models.ImageField('Фото', upload_to='brands', blank=True, default='noimage.jpg')
-    country = models.CharField('Страна', max_length=250)
-    description = models.TextField('Описание', blank=True, default='')
+    name = models.CharField(_('name'), max_length=250)
+    img = models.ImageField(_('photo'), upload_to='brands', blank=True, default='noimage.jpg')
+    country = models.CharField(_('country'), max_length=250)
+    description = models.TextField(_('description'), blank=True, default='')
 
     def __str__(self):
         return self.name
@@ -34,26 +34,26 @@ class Brands(models.Model):
 class Product(models.Model):
     class Meta:
         db_table = 'product'
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = _('Product')
+        verbose_name_plural = _('Products')
 
-    name = models.CharField('Название', max_length=250)
+    name = models.CharField(_('Name'), max_length=250)
     title = models.CharField('Title', max_length=250, blank=True, default='')
     description = models.TextField('Description', blank=True, default='')
-    category = models.IntegerField('Категория')
-    brand = models.IntegerField('Бренд')
-    parent = models.IntegerField('Основной товар', blank=True, default=0)
-    img = models.ImageField('Превью', upload_to='clothes/thamb', blank=True, default='noimage.jpg')
-    slide = models.ImageField('Слайды', upload_to='clothes/slide', blank=True, default='noimage.jpg')
-    text = models.TextField('Текст', blank=True, default='')
-    currency = models.CharField('Валюта', max_length=10)
-    price = models.FloatField('Цена')
-    old_price = models.FloatField('Старая цена', blank=True, default=0)
-    visible = models.SmallIntegerField('Видимость на сайте', blank=True, default=1)
-    hit = models.SmallIntegerField('Хит продаж', blank=True, default=0)
-    new = models.SmallIntegerField('Новинка', blank=True, default=0)
-    sale = models.SmallIntegerField('Распродажа', blank=True, default=0)
-    date = models.DateField('Дата добавления', auto_now_add=True)
+    category = models.IntegerField(_('Category'))
+    brand = models.IntegerField(_('Brand'))
+    parent = models.IntegerField(_('Base product'), blank=True, default=0)
+    img = models.ImageField(_('Preview'), upload_to='clothes/thamb', blank=True, default='noimage.jpg')
+    slide = models.ImageField(_('Slides'), upload_to='clothes/slide', blank=True, default='noimage.jpg')
+    text = models.TextField(_('Text'), blank=True, default='')
+    currency = models.CharField(_('Carrence'), max_length=10)
+    price = models.FloatField(_('Price'))
+    old_price = models.FloatField(_('Old price'), blank=True, default=0)
+    visible = models.SmallIntegerField(_('Viseble'), blank=True, default=1)
+    hit = models.SmallIntegerField(_('Hit'), blank=True, default=0)
+    new = models.SmallIntegerField(_('New'), blank=True, default=0)
+    sale = models.SmallIntegerField(_('Sale'), blank=True, default=0)
+    date = models.DateField(_('Date'), auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -62,19 +62,19 @@ class Product(models.Model):
 class Attr(models.Model):
     class Meta:
         db_table = 'attr'
-        verbose_name = 'Атрибут'
-        verbose_name_plural = 'Атрибуты'
+        verbose_name = _('Atribute')
+        verbose_name_plural = _('Atributes')
 
-    name = models.CharField('Имя', max_length=250)
+    name = models.CharField(_('Name'), max_length=250)
 
 
 
 class Options(models.Model):
     class Meta:
         db_table = 'options'
-        verbose_name = 'Опция'
-        verbose_name_plural = 'Опции'
+        verbose_name = _('Option')
+        verbose_name_plural = _('Options')
 
-    product = models.IntegerField('Товар', blank=True, default=0)
-    attr = models.IntegerField('Атрибут', blank=True, default=0)
-    value = models.CharField('Значение', max_length=250)
+    product = models.IntegerField(_('Product'), blank=True, default=0)
+    attr = models.IntegerField(_('Atribute'), blank=True, default=0)
+    value = models.CharField(_('Value'), max_length=250)
